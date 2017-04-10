e5f5b6146ce8e8e2a3cf2f24186d5affcdaf63c1
Alex M <mccalex1@umbc.edu>
2017-04-10 15:37:59 -0400

    divs hiding and opening

--- proj2.js
+++ proj2.js
@@ -1,1 +1,17 @@
 //javacsript file
+function travelTrail(){
+	document.getElementById("oregonTrail").style.display = "none";
+	document.getElementById("travelTrail").style.display = "block";
+}
+function learnMore(){
+	document.getElementById("oregonTrail").style.display = "none";
+	document.getElementById("learnMore").style.display = "block";
+}
+function seeTopTen(){
+	document.getElementById("oregonTrail").style.display = "none";
+	document.getElementById("seeTopTen").style.display = "block";
+}
+function managementOptions(){
+	document.getElementById("oregonTrail").style.display = "none";
+	document.getElementById("managementOptions").style.display = "block";
+}
