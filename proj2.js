1e467c2a7f0204c156a6c82e9982afe925ff0ee9
Alex M <mccalex1@umbc.edu>
2017-04-10 17:46:09 -0400

    high score page

--- proj2.js
+++ proj2.js
@@ -8,3 +8,19 @@ function backToOregonTrail(currentDivId){
 	document.getElementById(currentDivId).style.display = "none";
 	document.getElementById("oregonTrail").style.display = "block";
 }
+
+
+//onload function should set up and data that needs to be pulled from js file
+function getData(){
+	setUpHighScores();
+}
+
+function setUpHighScores(){
+	var names = [];
+	var scores = [];
+
+	document.getElementById("highScores");
+
+	//implement the 10 high score loop here
+	
+}
