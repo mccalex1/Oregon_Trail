0b85efb7a0699c7270c159f4269a0b22d67bdd5a
Alex M <mccalex1@umbc.edu>
2017-04-12 13:44:10 -0400

    commit

--- inClass/intro.js
+++ inClass/intro.js
@@ -1,5 +1,16 @@
+var currentMile = 10;
+
 function continueButton(currentDiv, nextDivId){
 	document.getElementById(currentDiv).style.display = "none";
 
 	document.getElementById(nextDivId).style.display = "block";
+}
+
+function changeInner(){
+	var inner = document.getElementById("milesToGo");
+	var inner2 = document.getElementById("milesTraveled");
+
+	currentMile -= 1;
+	inner.innerHTML = currentMile + " miles to go!";
+	inner2.innerHTML = 10 - currentMile + "miles traveled";
 }
